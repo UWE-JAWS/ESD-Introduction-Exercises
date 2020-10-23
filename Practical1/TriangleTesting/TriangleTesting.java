3955ff7e5e31620234622b1a795c2d4e1f99c133
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-10-23 17:09:28 +0100

    Add empty project structure, as far as practical 1 Each exercise is a main class inside its own sub-package of the Practical1 package

---
+++ Practical1/TriangleTesting/TriangleTesting.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author joshua
+ */
+public class TriangleTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
