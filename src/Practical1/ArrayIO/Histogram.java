1703c03cae1c63648548a96dce63f7e51b4ae19a
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-11-02 11:23:36 +0000

    Implement histogram visualisation

--- src/Practical1/ArrayIO/Histogram.java
+++ src/Practical1/ArrayIO/Histogram.java
@@ -25,7 +25,15 @@ public class Histogram {
     }
     
     public String getVisualisation() {
-        return null;
+        String output = "";
+        for (int i = 0; i < 10; i++) {
+            output += String.format("%2d - %3d | ", i * 10 + 1, (i + 1) * 10);
+            for (int t = 0; t < this.histogram[i]; t++) {
+                output += "*";
+            }
+            output += "\n";
+        }
+        return output;
     }
     
     private int[] histogram;  // the number range categories
