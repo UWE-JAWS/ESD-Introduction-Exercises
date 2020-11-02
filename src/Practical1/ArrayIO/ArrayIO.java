19552318e3670fb23a5573867543d8b290dcf02e
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-11-02 11:39:04 +0000

    Finished test of Histogram with random data generated in memory

--- src/Practical1/ArrayIO/ArrayIO.java
+++ src/Practical1/ArrayIO/ArrayIO.java
@@ -5,6 +5,10 @@
  */
 package Practical1.ArrayIO;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author Anon
@@ -15,7 +19,17 @@ public class ArrayIO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Histogram h = new Histogram(generateRandomData(100));
+        System.out.print(h.getVisualisation());
+    }
+    
+    private static List<Integer> generateRandomData(int size) {
+        Random prng = new Random();
+        List<Integer> data = new ArrayList<>();
+        for (int d : prng.ints(size, 1, 101).toArray()) {
+            data.add(d);
+        }
+        return data;
     }
     
 }
